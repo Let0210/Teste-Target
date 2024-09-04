8cbe2161573961613feb12505a7f5648995d2f10
Let0210 <oliveirarodriguesleticiaana@gmail.com>
2024-09-03 21:56:44 -0300

    add questão4.py

---
+++ questão 4.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+data = [
+	{
+		"estado": "SP",
+		"fatur_mensal": 67836.43
+	},
+	{
+		"estado": "RJ",
+		"fatur_mensal": 36678.66
+	},
+	{
+		"estado": "MG",
+		"fatur_mensal": 29229.88
+	},
+	{
+		"estado": "ES",
+		"fatur_mensal": 27165.48
+	},
+	{
+		"estado": "Outros",
+		"fatur_mensal": 19849.53
+	}
+]
+
+revenue_total = sum(uf['fatur_mensal'] for uf in data)
+
+for uf in data:
+    uf['percentual'] = (uf["fatur_mensal"]/revenue_total) * 100
+
+
+slices = [uf['estado'] for uf in data]
+percentages = [uf['percentual'] for uf in data]
+
+plt.figure(figsize=(8,8))
+plt.pie(percentages, labels=slices, autopct='%1.2f%%')
+plt.title('Faturamento mensal por estado em %')
+plt.show()
